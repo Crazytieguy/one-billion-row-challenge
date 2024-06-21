0bf6e23388fec97846b85157501a93e409e575c6
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-06-21 16:27:40 -0700

    initial commit

---
+++ src/bin/create_measurements.rs
@@ -0,0 +1,117 @@
+use anyhow::{anyhow, Context, Result};
+use clap::Parser;
+use humansize::{format_size, BINARY};
+use indicatif::ProgressBar;
+use rand::prelude::*;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::path::PathBuf;
+use std::time::Instant;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of records to create
+    #[arg(help = "Positive integer number of records to create")]
+    num_records: usize,
+
+    /// Input file containing weather station names
+    #[arg(short, long, help = "Path to the input weather stations file")]
+    input_file: PathBuf,
+
+    /// Output file for measurements
+    #[arg(short, long, help = "Path to the output measurements file")]
+    output_file: PathBuf,
+}
+
+fn build_weather_station_name_list(input_file: &PathBuf) -> Result<Vec<String>> {
+    let file = File::open(input_file)
+        .with_context(|| format!("Failed to open weather stations file: {:?}", input_file))?;
+    let reader = BufReader::new(file);
+
+    Ok(reader
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|line| !line.contains('#'))
+        .filter_map(|line| {
+            let (name, _) = line.split_once(';')?;
+            Some(name.to_string())
+        })
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect())
+}
+
+fn estimate_file_size(weather_station_names: &[String], num_rows_to_create: usize) -> String {
+    let total_name_bytes: usize = weather_station_names.iter().map(|s| s.len()).sum();
+    let avg_name_bytes = total_name_bytes as f64 / weather_station_names.len() as f64;
+    let avg_temp_bytes = 4.400200100050025;
+    let avg_line_length = avg_name_bytes + avg_temp_bytes + 2.0;
+    let estimated_size = num_rows_to_create as f64 * avg_line_length;
+
+    format_size(estimated_size as u64, BINARY)
+}
+
+use std::io::BufWriter;
+
+fn build_test_data(
+    weather_station_names: &[String],
+    num_rows_to_create: usize,
+    output_file: &PathBuf,
+) -> Result<()> {
+    let start_time = Instant::now();
+    let coldest_temp = -99.9;
+    let hottest_temp = 99.9;
+    let mut rng = rand::thread_rng();
+    let station_names_10k_max: Vec<_> = weather_station_names
+        .choose_multiple(&mut rng, 10_000)
+        .collect();
+
+    eprintln!("Building test data...");
+
+    let file = File::create(output_file)
+        .with_context(|| format!("Failed to create measurements file: {:?}", output_file))?;
+    let mut writer = BufWriter::new(file);
+
+    let pb = ProgressBar::new(num_rows_to_create as u64);
+
+    for i in 0..num_rows_to_create {
+        let station = station_names_10k_max
+            .choose(&mut rng)
+            .ok_or_else(|| anyhow!("Failed to choose a random station"))?;
+        let temp = rng.gen_range(coldest_temp..=hottest_temp);
+        writeln!(writer, "{};{:.1}", station, temp)
+            .context("Failed to write to measurements file")?;
+
+        if i % 10000 == 0 {
+            pb.set_position(i as u64);
+        }
+    }
+
+    writer.flush().context("Failed to flush writer")?;
+    pb.finish_with_message("Test data generation complete");
+
+    let elapsed_time = start_time.elapsed();
+    let file_size = std::fs::metadata(output_file)?.len();
+    let human_file_size = format_size(file_size, BINARY);
+
+    eprintln!("Test data successfully written to {:?}", output_file);
+    eprintln!("Actual file size: {}", human_file_size);
+    eprintln!("Elapsed time: {:?}", elapsed_time);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let weather_station_names = build_weather_station_name_list(&args.input_file)?;
+    let estimated_file_size = estimate_file_size(&weather_station_names, args.num_records);
+    eprintln!("Estimated file size is: {estimated_file_size}");
+
+    build_test_data(&weather_station_names, args.num_records, &args.output_file)?;
+    eprintln!("Test data build complete.");
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,166 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
+
+use clap::Parser;
+use fxhash::FxHashMap;
+use memmap2::Mmap;
+use rayon::{iter::ParallelIterator, slice::ParallelSlice};
+
+fn main() -> anyhow::Result<()> {
+    let start = std::time::Instant::now();
+    let args = Args::parse();
+    let file = File::open(&args.input_file)?;
+    let content = unsafe { Mmap::map(&file)? };
+    let content_slice = if content[content.len() - 1] == b'\n' {
+        &content[..content.len() - 1]
+    } else {
+        &content
+    };
+    eprintln!("Finished setup after {:?}", start.elapsed());
+    let start_parsing = std::time::Instant::now();
+    let registry = content_slice
+        .par_split(|&b| b == b'\n')
+        .fold(Registry::default, |mut registry, line| {
+            let (name, temp) = parse_line(line);
+            registry
+                .entry(name)
+                .or_insert_with(Aggregation::new)
+                .update(temp);
+            registry
+        })
+        .reduce(Registry::default, |mut a, b| {
+            b.into_iter().for_each(|(name, aggregation)| {
+                a.entry(name)
+                    .or_insert_with(Aggregation::new)
+                    .merge(&aggregation);
+            });
+            a
+        });
+    let elapsed = start_parsing.elapsed();
+    eprintln!("Finished aggregating after {:?}", elapsed);
+
+    let start_sorting = std::time::Instant::now();
+    let mut name_aggregations = registry.into_iter().collect::<Vec<_>>();
+    name_aggregations.sort_unstable_by_key(|&(name, _)| name);
+    let elapsed = start_sorting.elapsed();
+    eprintln!("Finished sorting after {:?}", elapsed);
+
+    let handle = std::io::stdout().lock();
+    let mut writer = BufWriter::new(handle);
+
+    let start_writing = std::time::Instant::now();
+    writer.write_all(b"{")?;
+    let (first_name, first_aggregation) = name_aggregations.first().unwrap();
+    push_aggregation(&mut writer, first_name, first_aggregation)?;
+    for (name, aggregation) in &name_aggregations[1..] {
+        writer.write_all(b", ")?;
+        push_aggregation(&mut writer, name, aggregation)?;
+    }
+    writer.write_all(b"}")?;
+    let elapsed = start_writing.elapsed();
+    eprintln!("Finished writing after {:?}", elapsed);
+
+    Ok(())
+}
+
+fn parse_line(line: &[u8]) -> (&[u8], i32) {
+    let (name, is_negative, tens, ones, decimal) = match line {
+        [name @ .., b';', b'-', tens, ones, b'.', decimal] => (name, true, *tens, *ones, *decimal),
+        [name @ .., b';', b'-', ones, b'.', decimal] => (name, true, b'0', *ones, *decimal),
+        [name @ .., b';', tens, ones, b'.', decimal] => (name, false, *tens, *ones, *decimal),
+        [name @ .., b';', ones, b'.', decimal] => (name, false, b'0', *ones, *decimal),
+        _ => panic!("Invalid line format {}", String::from_utf8_lossy(line)),
+    };
+    let zero = b'0' as i32;
+    let value =
+        ((tens as i32 - zero) * 100) + ((ones as i32 - zero) * 10) + (decimal as i32 - zero);
+    if is_negative {
+        (name, -value)
+    } else {
+        (name, value)
+    }
+}
+
+fn push_aggregation(
+    writer: &mut impl Write,
+    name: &[u8],
+    aggregation: &Aggregation,
+) -> anyhow::Result<()> {
+    writer.write_all(name)?;
+    writer.write_all(b"=")?;
+    push_float(writer, aggregation.min)?;
+    writer.write_all(b"/")?;
+    push_float(writer, aggregation.mean())?;
+    writer.write_all(b"/")?;
+    push_float(writer, aggregation.max)?;
+    Ok(())
+}
+
+fn push_float(writer: &mut impl Write, mut value: i32) -> anyhow::Result<()> {
+    if value < 0 {
+        writer.write_all(b"-")?;
+        value = -value;
+    }
+    if value >= 100 {
+        writer.write_all(&[(value / 100) as u8 + b'0'])?;
+    }
+    writer.write_all(&[((value / 10) % 10) as u8 + b'0'])?;
+    writer.write_all(b".")?;
+    writer.write_all(&[(value % 10) as u8 + b'0'])?;
+    Ok(())
+}
+
+type Registry<'a> = FxHashMap<&'a [u8], Aggregation>;
+
+struct Aggregation {
+    min: i32,
+    max: i32,
+    sum: i32,
+    count: u32,
+}
+
+impl Aggregation {
+    fn new() -> Self {
+        Self {
+            min: i32::MAX,
+            max: i32::MIN,
+            sum: 0,
+            count: 0,
+        }
+    }
+
+    fn update(&mut self, value: i32) {
+        self.min = self.min.min(value);
+        self.max = self.max.max(value);
+        self.sum += value;
+        self.count += 1;
+    }
+
+    fn merge(&mut self, other: &Self) {
+        self.min = self.min.min(other.min);
+        self.max = self.max.max(other.max);
+        self.sum += other.sum;
+        self.count += other.count;
+    }
+
+    fn mean(&self) -> i32 {
+        let mean_10 = self.sum * 10 / self.count as i32;
+        let remainder = mean_10 % 10;
+        if remainder >= 5 {
+            mean_10 / 10 + 1
+        } else {
+            mean_10 / 10
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to measurements file
+    #[arg(short, long)]
+    input_file: PathBuf,
+}
